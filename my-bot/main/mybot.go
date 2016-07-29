39c701b484bf842a29d9dac31131fd5e2217374e
Yuval Marcus <ymarcus93@gmail.com>
2016-07-29 10:17:08 -0400

    Added my work from golang meetup on 7/27

--- my-bot/main/mybot.go
+++ my-bot/main/mybot.go
@@ -8,25 +8,46 @@ import (
 	"golang.org/x/net/context"
 
 	slackbot "github.com/BeepBoopHQ/go-slackbot"
-	"github.com/briandowns/openweathermap"
 	"github.com/nlopes/slack"
+	"github.com/sparrc/go-ping"
 )
 
 func main() {
-	ListenForWeather()
+	Listen()
 }
 
-func GetWeather(place string) (*openweathermap.CurrentWeatherData, error) {
-	// TODO: get the weather for the place
-	return nil, nil
+func CheckServer(ip string) bool {
+	fmt.Println("DEBUG: Passed IP: ", ip)
+
+	pinger, err := ping.NewPinger(ip)
+	if err != nil {
+		panic(err)
+	}
+	pinger.Count = 3
+	pinger.Run()                 // blocks until finished
+	stats := pinger.Statistics() // get send/receive/rtt stats
+	if stats != nil {
+		return true
+	} else {
+		return false
+	}
 }
 
-func ListenForWeather() {
+func Listen() {
 	bot := slackbot.New(os.Getenv("SLACK_API_TOKEN"))
-	// TODO: add a bot.Hear listener here for a `weather <place>` request
+	bot.Hear("server (.*)").MessageHandler(RespondRequestHandler)
 	bot.Run()
 }
 
-func WeatherHandler(ctx context.Context, bot *slackbot.Bot, evt *slack.MessageEvent) {
-	// TODO: Respond to a `weather <place>` request
+func RespondRequestHandler(ctx context.Context, bot *slackbot.Bot, evt *slack.MessageEvent) {
+	// TODO: Respond to a `server <IP>` request
+	fmt.Println("DEBUG: User sent: ", evt.Msg.Text)
+	arrOfWords := strings.Fields(evt.Msg.Text)
+	ip := arrOfWords[len(arrOfWords)-1]
+	err := CheckServer(ip)
+	if err == true {
+		bot.Reply(evt, fmt.Sprintf("ONLINE"), slackbot.WithTyping)
+	} else {
+		bot.Reply(evt, fmt.Sprintf("OFFLINE"), slackbot.WithTyping)
+	}
 }
